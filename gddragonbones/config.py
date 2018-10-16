55ed8db5d4b764867cf1d7afe74b734b28441313
Damien Picard <dam.pic@free.fr>
2018-10-16 21:10:29 +0200

    Add class documentation

--- gddragonbones/config.py
+++ gddragonbones/config.py
@@ -2,10 +2,16 @@
 
 def can_build(platform):
   return True
-  
-  
+
+
 def configure(env):
   pass
-  
-  
-  
+
+
+def get_doc_classes():
+    return [
+        "GDDragonBones",
+    ]
+
+def get_doc_path():
+    return "doc_classes"
